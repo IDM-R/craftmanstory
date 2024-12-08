56aa7148791f02d8512062a29b50f1ce7ebbf8cc
TomoyaKinami <8921056@ed.tus.ac.jp>
2024-12-08 18:08:19 +0900

    英語版岩井製作所を作っている

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask,render_template
 app = Flask(__name__, static_url_path="/")
 
 vol_list = ["0","1"]
-en_list = ["0"]
+en_list = ["0","1"]
 
 # トップページ
 @app.route("/",methods = ["GET"])

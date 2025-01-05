e9ead9cf8c164d25098d67f23c4c4ed18693f2b3
TomoyaKinami <8921056@ed.tus.ac.jp>
2025-01-05 15:48:37 +0900

    超重要な修正

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from flask import Flask,render_template
 
 app = Flask(__name__, static_url_path="/")
 
-vol_list = ["0","1"]
-en_list = ["0","1"]
+vol_list = ["0","1","2"]
+en_list = ["0","1","2"]
 
 # トップページ
 @app.route("/",methods = ["GET"])
@@ -23,9 +23,10 @@ def vol0(num):
             break
         else:
             if i == len(vol_list)-1:
-                return render_template("404.html"),404
+                nf404 = True
+                return render_template("404.html",nf404 = nf404),404
             else:
-                break
+                continue
     return render_template("article"+num+".html",article = True)
 
 #「/vol/<num>/en」へアクセスがあった場合に、「article<vol>_en.html」を返す。英語版
@@ -39,7 +40,7 @@ def vol_en(num):
                 nf404 = True
                 return render_template("404.html",nf404 = nf404),404
             else:
-                break
+                continue
     return render_template("article"+num+"_en.html",article = True)
 
 
